d5b21a6a36123c34bd79a02913be0c51bbe97941
Mohamed Ramadan <mr01028760097@gmail.com>
2022-07-16 19:01:02 +0200

    scroll to place

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, createRef } from "react";
 import {
   CircularProgress,
   Grid,
@@ -11,54 +11,72 @@ import {
 import PlaceDetials from "./../PlaceDetails/PlaceDetials";
 
 import useStyles from "./styles";
-const List = ({ places }) => {
+const List = ({
+  places,
+  childClicked,
+  isLoading,
+  type,
+  setType,
+  rate,
+  setRate,
+}) => {
   const classes = useStyles();
-  const [type, setType] = useState("restaurants");
-  const [rate, setRate] = useState(0);
-  // const places = [
-  //   { name: "first place" },
-  //   { name: "second place" },
-  //   { name: "third place" },
-  //   { name: "first place" },
-  //   { name: "second place" },
-  //   { name: "third place" },
-  //   { name: "first place" },
-  //   { name: "second place" },
-  //   { name: "third place" },
-  //   { name: "first place" },
-  //   { name: "second place" },
-  //   { name: "third place" },
-  // ];
-  return (
-    <div className={classes.container}>
-      <Typography variant="h6">
-        Restaurants, Hotels & Attractions around you
-      </Typography>
-      <FormControl className={classes.formControl}>
-        <InputLabel>Type</InputLabel>
-        <Select value={type} onChange={(e) => setType(e.target.value)}>
-          <MenuItem value="restaurants">Restaurants</MenuItem>
-          <MenuItem value="hotels">Hotels</MenuItem>
-          <MenuItem value="attractions">Attractions</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel>Rating</InputLabel>
-        <Select value={rate} onChange={(e) => setRate(e.target.value)}>
-          <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={3}>Above 3.0</MenuItem>
-          <MenuItem value={4}>Above 4.0</MenuItem>
-          <MenuItem value={4.5}>Above 4.5</MenuItem>
-        </Select>
-      </FormControl>
-      <Grid container spacing={3} className={classes.list}>
-        {places?.map((place, i) => (
-          <Grid item key={i} xs={12}>
-            <PlaceDetials place={place} />
-          </Grid>
-        ))}
-      </Grid>
+  const [elRefs, setElRefs] = useState([]);
+
+  // useEffect(() => {
+  //   const refs = Array(places?.length).fill().map(
+  //     (_, i) => elRefs[i] || createRef()
+  //   );
+  //   setElRefs(refs);
+  // }, [places]);
+
+  useEffect(() => {
+    setElRefs((refs) =>
+      Array(places?.length)
+        .fill()
+        .map((_, i) => refs[i] || createRef())
+    );
+  }, [places]);
+  return isLoading ? (
+    <div className={classes.loading}>
+      <CircularProgress />
     </div>
+  ) : (
+    <>
+      <div className={classes.container}>
+        <Typography variant="h6">
+          Restaurants, Hotels & Attractions around you
+        </Typography>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Type</InputLabel>
+          <Select value={type} onChange={(e) => setType(e.target.value)}>
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Rating</InputLabel>
+          <Select value={rate} onChange={(e) => setRate(e.target.value)}>
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3.0</MenuItem>
+            <MenuItem value={4}>Above 4.0</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
+          </Select>
+        </FormControl>
+        <Grid container spacing={3} className={classes.list}>
+          {places?.map((place, i) => (
+            <Grid ref={elRefs[i]} item key={i} xs={12}>
+              <PlaceDetials
+                place={place}
+                refProp={elRefs[i]}
+                selected={Number(childClicked) === i}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </>
   );
 };
 
